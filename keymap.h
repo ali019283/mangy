fdc612e79f20ab69b63caadbdc388c44086f6994
Ali Burak Çetin <aliburakcetin@protonmail.com>
2023-09-23 03:27:26 +0300

    better code + shift usage

---
+++ keymap.h
@@ -0,0 +1,7 @@
+#define mod1 Mod1Mask
+#define killc 24 //q
+#define escwm 9 //esc
+#define mvl 43 //h
+#define mvd 44 //j
+#define mvu 45 //k
+#define mvr 46 //l
--- mangy.c
+++ mangy.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <X11/Xlib.h>
-#define modkey Mod4Mask
+#include "keymap.h"
 Display *disp;
 Window root;
 XWindowAttributes attr;
@@ -11,49 +11,49 @@ int mvrs(int x, int y, int mvx, int mvy){
 }
 int main(){
     if(!(disp = XOpenDisplay(0x0))){puts("cant open display"); return 1;}
-    int arr[]={9, 24, 43, 44, 45, 46, 111, 113, 114, 116};
+    int arr[]={escwm, killc, mvl, mvd, mvu, mvr,};
     root = DefaultRootWindow(disp);
     for (int i = 0; i <= 9; i++){
-        XGrabKey(disp, arr[i], modkey, root, True, GrabModeAsync, GrabModeAsync);
+        XGrabKey(disp, arr[i], mod1, root, True, GrabModeAsync, GrabModeAsync);
+	XGrabKey(disp, arr[i], mod1 | ShiftMask, root, True, GrabModeAsync, GrabModeAsync);
         printf("%d", arr[i]);
     }
     XGrabButton(disp, 1, AnyModifier, root, True, ButtonPressMask, GrabModeAsync,GrabModeAsync, None, None);
     while(1){
         XNextEvent(disp, &event);
         if(event.type == KeyPress){
-            if(event.xkey.keycode == 0x09){
+            if(event.xkey.keycode == escwm){
                 break;
             }
             if(event.xkey.subwindow != None){
                 switch(event.xkey.keycode){
-                    case 24:    
+                    case killc:    
                         XDestroyWindow(disp, event.xkey.subwindow);
                         break;
-                    case 43:
-                        mvrs(-30,0,0,0);
+                    case mvl:
+			if(event.xkey.state & ShiftMask)
+				mvrs(0,0,-50,0);
+			else 
+				mvrs(-30,0,0,0);
                         break;
-                    case 44:
-                        mvrs(0,30,0,0);
+                    case mvd:
+			if(event.xkey.state & ShiftMask)
+				mvrs(0,0,0,50);
+			else
+				mvrs(0,30,0,0);
                         break;
-                    case 45:
-                        mvrs(0,-30,0,0);
+                    case mvu:
+			if(event.xkey.state & ShiftMask)
+				mvrs(0,0,0,-50);
+			else
+                        	mvrs(0,-30,0,0);
                         break;
-                    case 46:
-                        mvrs(30,0,0,0);
+                    case mvr:
+			if(event.xkey.state & ShiftMask)
+				mvrs(0,0,50,0);
+			else
+                        	mvrs(30,0,0,0);
                         break;
-                    case 114:
-                        mvrs(0,0,50,0);
-                        break;
-                    case 111:
-                        mvrs(0,0,0,-50);
-                        break;
-                    case 113:
-                        mvrs(0,0,-50,0);
-                        break;
-                    case 116:
-                        mvrs(0,0,0,50);
-                        break;
-
                 }
             }
         }else if(event.type == ButtonPress && event.xbutton.subwindow != None){XRaiseWindow(disp, event.xbutton.subwindow);}
